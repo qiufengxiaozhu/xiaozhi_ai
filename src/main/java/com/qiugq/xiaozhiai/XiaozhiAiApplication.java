6388939491fcf4a41acc532d0a1f8e395d6f2988
qiugq <qiugq@filez.com>
2025-04-23 00:48:29 +0800

    AI项目初始化

---
+++ src/main/java/com/qiugq/xiaozhiai/XiaozhiAiApplication.java
@@ -0,0 +1,13 @@
+package com.qiugq.xiaozhiai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaozhiAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XiaozhiAiApplication.class, args);
+	}
+
+}
